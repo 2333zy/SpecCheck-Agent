65546dccfbe6589a9f72d9b22f50cf5fabb4d279
2333zy <9875933+xiaoyuxiaoyuzz@user.noreply.gitee.com>
2026-05-13 09:07:22 +0800

    chore: ignore package build artifacts

--- packages/core/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./schemas/acceptance";
-export * from "./security/commands";
-export * from "./report/report";
--- packages/core/src/security/commands.d.ts
+++
@@ -1,9 +0,0 @@
-export type CommandValidationResult = {
-    ok: true;
-    normalizedCommand: string;
-} | {
-    ok: false;
-    reason: string;
-};
-export declare function getCommandAllowlist(envValue?: string | undefined): string[];
-export declare function validateStartCommand(command: string, allowlist?: string[]): CommandValidationResult;
